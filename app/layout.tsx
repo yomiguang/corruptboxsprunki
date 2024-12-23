94c65880b38f10c778dc0b6e6af633a6e84efbb1
yomiguang <guangyomi@gmail.com>
2024-12-23 08:41:17 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,16 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Corruptbox-Play Sprunki Corruptbox Game Online Free',
   description: 'A revolutionary musical platform that fuses horror aesthetics with experimental music creation',
-  icons: {
-    icon: [
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon.ico', sizes: 'any' },
-    ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
-  },
+  // icons: {
+  //   icon: [
+  //     { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+  //     { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+  //     { url: '/favicon.ico', sizes: 'any' },
+  //   ],
+  //   apple: [
+  //     { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+  //   ],
+  // },
 }
 
 export default function RootLayout({
